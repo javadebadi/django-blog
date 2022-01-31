18792ece1c057979007571acfe80d72229a7ed86
javadebadi <javad.ebadi.1990@gmail.com>
2022-01-31 23:01:38 +0330

    Update html files to include static files. Adde new urls and view which just render the hard coded html templates.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ import blog.views as views
 
 urlpatterns = [
     path('', views.home, name='blog_home'),
+    path('about/', views.about, name='blog_about'),
+    path('post/', views.post, name='blog_post'),
+    path('contact/', views.contact, name='blog_contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'blog/index.html', context={})
+
+def about(request):
+    return render(request, 'blog/about.html', context={})
+
+def contact(request):
+    return render(request, 'blog/contact.html', context={})
+
+def post(request):
+    return render(request, 'blog/post.html', context={})
