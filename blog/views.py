0baa789e68e08faef6fc097f95d5cbdae791f149
javadebadi <javad.ebadi.1990@gmail.com>
2022-02-01 20:39:34 +0330

    Updated index.html template and view for home page to read posts from Post model

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/index.html', context={})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts':posts})
 
 def about(request):
     return render(request, 'blog/about.html', context={})
