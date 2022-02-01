7805265ca4f586319f46a1a1a5db0a6473eed662
javadebadi <javad.ebadi.1990@gmail.com>
2022-02-01 22:28:52 +0330

    Added post_detail and post_list views and updated urls and templates according to them

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ import blog.views as views
 urlpatterns = [
     path('', views.home, name='blog_home'),
     path('about/', views.about, name='blog_about'),
-    path('post/', views.post, name='blog_post'),
+    path('post/', views.post_list, name='blog_post_list'),
+    path('post/<int:pk>/', views.post_detail, name='blog_post_detail'),
     path('contact/', views.contact, name='blog_contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
@@ -12,5 +13,10 @@ def about(request):
 def contact(request):
     return render(request, 'blog/contact.html', context={})
 
-def post(request):
-    return render(request, 'blog/post.html', context={})
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts':posts})
+
+def post_detail(request, pk=None):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post.html', context={'post':post})
