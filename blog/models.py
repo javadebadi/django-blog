e5cd6d4860b27b99c3c3e08c49f9128139bae23b
javadebadi <javad.ebadi.1990@gmail.com>
2022-02-01 20:09:01 +0330

    Create Post model and run make migrations and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(
+        max_length=256,
+    )
+
+    subheading = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+
+    publish_date = models.DateField(
+        auto_now=True,
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_column='author_id',
+        related_name='posts',
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        db_table = 'post'
