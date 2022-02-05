b29b8d6a0b5b2ce04299dc78888ca4439564b9ff
javadebadi <javad.ebadi.1990@gmail.com>
2022-02-05 18:10:04 +0330

    Create contact message model

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import (
+    Post,
+    ContactMessage,
+    )
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publish_date')
     list_filter = ('author', 'publish_date')
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'insert_time')
+    list_filter = ('email',)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -41,4 +42,36 @@ class Post(models.Model):
         ordering = (
             '-publish_date',  # Descending -> New publish posts first
             '-id',
+        )
+
+
+# create model for contact message
+class ContactMessage(models.Model):
+
+    phone_number = models.CharField(
+        max_length=12,
+        null=True,
+        blank=True,
+        )
+    
+    email = models.EmailField()
+
+    name = models.CharField(max_length=128)
+
+    message = models.CharField(max_length=1500)
+
+    insert_time = models.DateTimeField(
+        auto_now_add=True,
+        )
+
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        db_table = 'contact_message'
+        verbose_name = 'Contact Message'
+        verbose_name_plural = 'Contact Messages'
+        ordering = (
+            '-insert_time',
         )
