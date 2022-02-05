29735e034a3a77ce181468b9db572145e6cc39fb
javadebadi <javad.ebadi.1990@gmail.com>
2022-02-05 18:45:29 +0330

    Update forms.py by adding labels for contact message form

--- blog/forms.py
+++ blog/forms.py
@@ -1,17 +1,23 @@
+from cProfile import label
 from django import forms
 
 class ContactMessageForm(forms.Form):
 
     phone_number = forms.CharField(
         max_length=12,
+        label='Phone number',
     )
 
-    email = forms.EmailField()
+    email = forms.EmailField(
+        label='Email',
+    )
 
     name = forms.CharField(
         max_length=128,
+        label='Name',
     )
 
     message = forms.CharField(
         max_length=1500,
+        label='Message',
     )
