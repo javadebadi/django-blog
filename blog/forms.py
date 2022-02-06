c6d0759796a593b9a40bf8be8ad174bb1c70c352
javadebadi <javad.ebadi.1990@gmail.com>
2022-02-06 19:52:22 +0330

    Update contac template and form to beautify form

--- blog/forms.py
+++ blog/forms.py
@@ -6,18 +6,43 @@ class ContactMessageForm(forms.Form):
     phone_number = forms.CharField(
         max_length=12,
         label='Phone number',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Phone Number',
+            }
+        )      
     )
 
     email = forms.EmailField(
         label='Email',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Email',
+            }
+        )      
     )
 
     name = forms.CharField(
         max_length=128,
         label='Name',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Name',
+            }
+        )      
     )
 
     message = forms.CharField(
         max_length=1500,
         label='Message',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'style': 'height: 12rem;',
+                'placeholder': 'Message',
+            }
+        )
     )
